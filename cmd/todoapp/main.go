f3f918f83e5a74e4be67e000fdf607d2a2ced257
mairi77 <Airi.Mayumi@jp.nttdata.com>
2024-07-19 14:58:41 +0900

    Update main.go

--- cmd/todoapp/main.go
+++ cmd/todoapp/main.go
@@ -1,41 +1,47 @@
 package main
 
 import (
-	"os"
-	"todoapp/internal/controller"
-	"todoapp/internal/repository"
+    "log"
+    "os"
+    "todoapp/internal/controller"
+    "todoapp/internal/repository"
 
-	"github.com/akrylysov/algnhsa" // GinフレームワークとLambdaを統合
-	"github.com/gin-gonic/gin"
+    "github.com/akrylysov/algnhsa"
+    "github.com/gin-gonic/gin"
 )
 
 func createRouter() *gin.Engine {
-	repository.InitDB()
+    log.Println("Initializing Database...")
+    repository.InitDB()
+    log.Println("Database Initialized.")
 
-	// Ginのデフォルト設定を使用
-	router := gin.Default()
+    router := gin.Default()
 
-	// ルートの定義
-	router.GET("/todo", controller.GetTodos)
-	router.POST("/todo", controller.CreateTodo)
-	router.PUT("/todo/:id", controller.UpdateTodo)
-	router.DELETE("/todo/:id", controller.DeleteTodo)
-	router.PUT("/todo/:id/finish", controller.FinishTodo)
-	router.GET("/todo/:id", controller.GetTodoByID)
-	router.GET("/todo/search", controller.SearchTodos)
+    router.GET("/todo", controller.GetTodos)
+    router.POST("/todo", controller.CreateTodo)
+    router.PUT("/todo/:id", controller.UpdateTodo)
+    router.DELETE("/todo/:id", controller.DeleteTodo)
+    router.PUT("/todo/:id/finish", controller.FinishTodo)
+    router.GET("/todo/:id", controller.GetTodoByID)
+    router.GET("/todo/search", controller.SearchTodos)
 
-	return router
+    log.Println("Routes registered.")
+
+    return router
 }
 
 func main() {
-	// コマンドライン引数に "lambda" が含まれていれば、Lambdaで実行されていると判断
-	if len(os.Args) > 1 && os.Args[1] == "lambda" {
-		router := createRouter()
-		algnhsa.ListenAndServe(router, &algnhsa.Options{
-			UseProxyPath: true,
-		})
-	} else {
-		router := createRouter()
-		router.Run(":8080") // ローカル環境で実行される場合
-	}
+    if len(os.Args) > 1 && os.Args[1] == "lambda" {
+        router := createRouter()
+        log.Println("Running in lambda mode.")
+        algnhsa.ListenAndServe(router, &algnhsa.Options{
+            UseProxyPath: true,
+        })
+    } else {
+        router := createRouter()
+        log.Println("Running in local mode.")
+        if err := router.Run(":8080"); err != nil {
+            log.Fatalf("Failed to run server: %v", err)
+        }
+    }
 }
