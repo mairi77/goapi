e4ed77724fff93cf4934fdf575ba0929dcb6605b
mairi77 <Airi.Mayumi@jp.nttdata.com>
2024-07-19 16:36:41 +0900

    Update main.go

--- cmd/todoapp/main.go
+++ cmd/todoapp/main.go
@@ -1,47 +1,49 @@
 package main
 
 import (
-    "log"
-    "os"
-    "todoapp/internal/controller"
-    "todoapp/internal/repository"
+	"log"
+	"os"
+	"todoapp/internal/controller"
+	"todoapp/internal/repository"
 
-    "github.com/akrylysov/algnhsa"
-    "github.com/gin-gonic/gin"
+	"github.com/akrylysov/algnhsa"
+	"github.com/gin-gonic/gin"
 )
 
 func createRouter() *gin.Engine {
-    log.Println("Initializing Database...")
-    repository.InitDB()
-    log.Println("Database Initialized.")
+	log.Println("Initializing Database...")
+	repository.InitDB()
+	log.Println("Database Initialized.")
 
-    router := gin.Default()
+	router := gin.Default()
 
-    router.GET("/todo", controller.GetTodos)
-    router.POST("/todo", controller.CreateTodo)
-    router.PUT("/todo/:id", controller.UpdateTodo)
-    router.DELETE("/todo/:id", controller.DeleteTodo)
-    router.PUT("/todo/:id/finish", controller.FinishTodo)
-    router.GET("/todo/:id", controller.GetTodoByID)
-    router.GET("/todo/search", controller.SearchTodos)
+	log.Println("Registering routes...")
+	router.GET("/todo", controller.GetTodos)
+	router.POST("/todo", controller.CreateTodo)
+	router.PUT("/todo/:id", controller.UpdateTodo)
+	router.DELETE("/todo/:id", controller.DeleteTodo)
+	router.PUT("/todo/:id/finish", controller.FinishTodo)
+	router.GET("/todo/:id", controller.GetTodoByID)
+	router.GET("/todo/search", controller.SearchTodos)
+	log.Println("Routes registered.")
 
-    log.Println("Routes registered.")
-
-    return router
+	return router
 }
 
 func main() {
-    if len(os.Args) > 1 && os.Args[1] == "lambda" {
-        router := createRouter()
-        log.Println("Running in lambda mode.")
-        algnhsa.ListenAndServe(router, &algnhsa.Options{
-            UseProxyPath: true,
-        })
-    } else {
-        router := createRouter()
-        log.Println("Running in local mode.")
-        if err := router.Run(":8080"); err != nil {
-            log.Fatalf("Failed to run server: %v", err)
-        }
-    }
+	log.Println("Starting application...")
+
+	if os.Getenv("AWS_LAMBDA_FUNCTION_NAME") != "" {
+		router := createRouter()
+		log.Println("Running in lambda mode.")
+		algnhsa.ListenAndServe(router, &algnhsa.Options{
+			UseProxyPath: true,
+		})
+	} else {
+		router := createRouter()
+		log.Println("Running in local mode.")
+		if err := router.Run(":8080"); err != nil {
+			log.Fatalf("Failed to run server: %v", err)
+		}
+	}
 }
