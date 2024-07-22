c4043139cb8a69393c1cabe1fff13888dc701a72
mairi77 <Airi.Mayumi@jp.nttdata.com>
2024-07-22 11:20:03 +0900

    Update main.go

--- cmd/todoapp/main.go
+++ cmd/todoapp/main.go
@@ -15,9 +15,15 @@ func createRouter() *gin.Engine {
 	repository.InitDB()
 	log.Println("Database Initialized.")
 
-	router := gin.Default()
+	router := gin.New() // DefaultをNewに変更してカスタムミドルウェアを追加
+	router.Use(gin.Logger(), gin.Recovery())
 
 	log.Println("Registering routes...")
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Welcome to the Todo App API!",
+		})
+	})
 	router.GET("/todo", controller.GetTodos)
 	router.POST("/todo", controller.CreateTodo)
 	router.PUT("/todo/:id", controller.UpdateTodo)
@@ -36,9 +42,12 @@ func main() {
 	if os.Getenv("AWS_LAMBDA_FUNCTION_NAME") != "" {
 		router := createRouter()
 		log.Println("Running in lambda mode.")
+
+		// 利用可能なフィールドをデフォルトのListenAndServeとして呼び出し
 		algnhsa.ListenAndServe(router, &algnhsa.Options{
 			UseProxyPath: true,
 		})
+
 	} else {
 		router := createRouter()
 		log.Println("Running in local mode.")
